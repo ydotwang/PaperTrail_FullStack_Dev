88a4810e0b2a1190e18edf37b2cecdb0cbf6b936
Yuyang Wang <wangyy10081008@gmail.com>
2025-02-11 23:38:31 -0600

    finished navbar

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+// import { Toolbar } from "@/components/toolbar";
+// import { Cover } from "@/components/cover";
+import { useMemo } from "react";
+import dynamic from "next/dynamic";
+import { useParams } from "next/navigation";
+
+export default function DocumentIdPage() {
+//   const { documentId } = useParams();
+//   const Editor = useMemo(
+//     () => dynamic(() => import("@/components/editor"), { ssr: false }),
+//     [],
+//   );
+
+//   // @ts-ignore
+//   const document = useQuery(api.documents.getById, { documentId: documentId });
+
+//   const update = useMutation(api.documents.update);
+
+//   const onChange = (content: string) => {
+//     // @ts-ignore
+//     update({ id: documentId, content });
+//   };
+
+//   if (document === undefined) {
+//     return (
+//       <div>
+//         <Cover.Skeleton />
+//         <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+//           <div className="space-y-4 pl-8 pt-4">
+//             <Skeleton className="h-14 w-[50%]" />
+//             <Skeleton className="h-4 w-[80%]" />
+//             <Skeleton className="h-4 w-[40%]" />
+//             <Skeleton className="h-4 w-[60%]" />
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   if (document === null) {
+//     return <div>Not found</div>;
+//   }
+
+//   return (
+//     <div className="pb-40">
+//       <Cover url={document.coverImage} />
+//       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+//         <Toolbar initialData={document} />
+//         <Editor onChange={onChange} initialContent={document.content} />
+//       </div>
+//     </div>
+//   );
+
+return <div>Hello</div>;
+}
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,7 +4,7 @@ import { Plus } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { ChevronsLeft, ChevronsRight, MenuIcon, PlusCircle, Search, Settings, Trash } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useCallback, useEffect, useRef } from "react";
 import { useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -18,6 +18,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { TrashBox } from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
+import { Navbar } from "./navbar";
 
 
 
@@ -27,6 +28,7 @@ export const Navigation = () => {
     const create = useMutation(api.documents.create);
     const search = useSearch();
     const settings = useSettings();
+    const params = useParams();
     
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -191,7 +193,12 @@ export const Navigation = () => {
             isMobile && "left-0 w-full",
 
             )}>
-            <nav className="bg-transparent px-3 py-2 w-full">
+                {!!params.documentId ? (
+                    <Navbar 
+                    isCollapsed={isCollapsed} 
+                    onResetWidth={resetWidth}/>
+                ) : (
+                    <nav className="bg-transparent px-3 py-2 w-full">
                 {isCollapsed && (
                     <MenuIcon 
                     onClick={() => resetWidth()}
@@ -199,6 +206,8 @@ export const Navigation = () => {
                     className="w-6 h-6 text-muted-foreground" />
                 )}
             </nav>
+                )}
+
 
         </div>
         </>
