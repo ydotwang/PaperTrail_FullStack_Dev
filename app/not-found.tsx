a5f78d12b00ccb5a0cc61640823210af214813eb
Yuyang Wang <wangyy10081008@gmail.com>
2025-04-17 02:42:31 -0500

    not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,24 +1,5 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Page Not Found",
-  description: "The page you are looking for does not exist.",
-};
+import { notFound } from "next/navigation";
 
 export default function NotFound() {
-  return (
-    <div className="h-full flex flex-col items-center justify-center space-y-4">
-      <h2 className="text-xl font-medium">Page not found</h2>
-      <p className="text-muted-foreground text-sm">
-        Could not find the requested page
-      </p>
-      <Button asChild>
-        <Link href="/documents">
-          Go back to documents
-        </Link>
-      </Button>
-    </div>
-  );
+  notFound();
 } 
