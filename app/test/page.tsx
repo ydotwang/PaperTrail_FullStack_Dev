e22a0834629abd0244faa86d79cd9197695b1f36
Yuyang Wang <wangyy10081008@gmail.com>
2025-04-17 02:37:21 -0500

    fix

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,14 @@
-const TestPage = () => {
-    return (  
-        <div>
-            <h1>Test Page</h1>
-        </div>
-    );
-}
+import { Metadata } from "next";
 
-export default TestPage;
+export const metadata: Metadata = {
+  title: "Test Page",
+  description: "A test page for development purposes",
+};
+
+export default function TestPage() {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">Test Page</h1>
+    </div>
+  );
+}
